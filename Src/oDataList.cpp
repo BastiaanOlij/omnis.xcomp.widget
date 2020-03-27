d5fa1d93fe90ea337073b087dec942980fca6232
Bastiaan Olij <mux213@gmail.com>
2020-03-27 15:32:49 +1100

    Fixing crash issue around drag and drop on PC

--- Src/oDataList.cpp
+++ Src/oDataList.cpp
@@ -1458,7 +1458,7 @@ void	oDataList::evMouseMoved(qpoint pMovedTo) {
 			mColumnWidths.setElementAtIndex(mMouseHitTest.mColNo, newWidth);
 			WNDinvalidateRect(mHWnd, NULL);
 			
-			ECOsendEvent(mHWnd, oDL_evColumnResized, 0, 0, EEN_EXEC_IMMEDIATE);
+			ECOsendEvent(mHWnd, oDL_evColumnResized, 0, 0, EEN_EXEC_LATER);
 		};
 		mMouseLast = pMovedTo;
 	};
@@ -1516,7 +1516,7 @@ void	oDataList::evClick(qpoint pAt, EXTCompInfo* pECI) {
                     // let user know we clicked outside of our line
                     EXTfldval	evParam[1];
                     evParam[0].setLong(0);
-                    ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_IMMEDIATE);
+                    ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_LATER);
                 };
 			}; break;
 			case oDL_row: {
@@ -1576,7 +1576,7 @@ void	oDataList::evClick(qpoint pAt, EXTCompInfo* pECI) {
                     // let user know we clicked on a line
                     EXTfldval	evParam[1];
                     evParam[0].setLong(mMouseHitTest.mLineNo);
-                    ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_IMMEDIATE);
+                    ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_LATER);
                 };
 			};	break;
 			default:
@@ -1608,7 +1608,7 @@ bool	oDataList::evDoubleClick(qpoint pAt, EXTCompInfo* pECI) {
 				// maybe send a click event back to Omnis?
 			};	break;
 			case oDL_row: {
-				ECOsendEvent(mHWnd, oDL_evDoubleClick, 0, 0, EEN_EXEC_IMMEDIATE);				
+				ECOsendEvent(mHWnd, oDL_evDoubleClick, 0, 0, EEN_EXEC_LATER);				
 			};	break;
 			default:
 				break;
@@ -1672,7 +1672,7 @@ bool	oDataList::evMouseRDown(qpoint pDownAt, EXTCompInfo* pECI) {
 			// let user know we changed our selection by simulating a click on a line
 			EXTfldval	evParam[1];
 			evParam[0].setLong(newCurrentLine);
-			ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_IMMEDIATE);				
+			ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_LATER);				
 		};
 	};
 
@@ -1715,7 +1715,7 @@ bool	oDataList::evKeyPressed(qkey *pKey, bool pDown, EXTCompInfo* pECI) {
             // and finally treat this as a click
 			EXTfldval	evParam[1];
 			evParam[0].setLong(1);
-			ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_IMMEDIATE);
+			ECOsendEvent(mHWnd, oDL_evClick, evParam, 1, EEN_EXEC_LATER);
 			
 			return true;
 		} else {
@@ -1774,14 +1774,16 @@ qlong	oDataList::evSetDragValue(FLDdragDrop *pDragInfo, EXTCompInfo* pECI) {
             width += mColumnWidths[i];
         };
         if (width < 100) width = 100;
+
+        /*
         qdim left	= pDragInfo->mStartPoint.h - (width/2);
         qdim top	= pDragInfo->mStartPoint.v - (height/2);
 	
-        // will this get destroyed by omnis?? if not we may want to solve this differently...
-        qrgn *region = new qrgn;
+        // this crashes, there is a fix in 10.2 coming
+        qrgn *region = GDIallocQRGN();
         GDIsetRectRgn(region, left, top, left+width, top+32);
 
-        /* free any shape Omnis may have set, we're replacing it with our own*/
+        // free any shape Omnis may have set, we're replacing it with our own
         if (pDragInfo->mDragShape!=0) {
             delete pDragInfo->mDragShape;
             pDragInfo->mDragShape = 0;
@@ -1789,6 +1791,24 @@ qlong	oDataList::evSetDragValue(FLDdragDrop *pDragInfo, EXTCompInfo* pECI) {
 
         pDragInfo->mDragShape = region;
         pDragInfo->mAllowsBitmapDragging = 0;
+
+        */
+
+		HDC tmpDC;
+		GDIcreateAlphaDC(&tmpDC, width, height);
+		qrect r(0, 0, width - 1, height - 1);
+
+		// Draw the alpha bitmap - for now we do a simple shape, we should really draw our row in here.
+		HPEN pen = GDIcreatePen(1, 0x8800FF00, patFill);
+		HPEN oldPen = GDIselectObject(tmpDC, pen);
+		GDIframeRect(tmpDC, &r);
+		GDIselectObject(tmpDC, oldPen);
+		GDIdeleteObject(pen);
+
+		pDragInfo->mBitmapBounds = r;
+		GDIdeleteAlphaDC(tmpDC, &pDragInfo->mDragBitmap);      // This deletes HDC and creates an alpha bitmap containing what was drawn into HDC
+		pDragInfo->mDragBitmapMask =  (HBITMAPMASK)0x02; // In 10.2 (HBITMAPMASK)0x02 will be a constant HBITMAP_ISALPHA
+		pDragInfo->mAllowsBitmapDragging = qtrue;
 	
         EXTfldval	dragType(pDragInfo->mDragType);
         dragType.setLong(cFLDdragDrop_dragData);
